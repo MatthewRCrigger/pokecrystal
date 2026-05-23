4a29896d22c08151d3679ad4fedeb9c3d49bf57a
Matthew Crigger <mrc@matthewrcrigger.com>
2026-05-23 14:48:52 -0400

    Fix

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2166,23 +2166,6 @@ ApplyBattleExperience:
 	ret z
 	ld hl, wEnemyMonBaseExp
 	srl [hl]
-	inc hl
-	dec b
-	jr nz, .loop
-
-ApplyExperienceAfterEnemyCaught:
-	; Preserve bits of non-fainted participants
-	ld a, [wBattleParticipantsNotFainted]
-	ld d, a
-	push de
-	call GiveExperiencePoints
-	pop de
-	; If Exp. Share is ON, give 50% EXP to non-participants
-	ld a, [wExpShareToggle]
-	and a
-	ret z
-	ld hl, wEnemyMonBaseExp
-	srl [hl]
 	ld a, [wBattleParticipantsNotFainted]
 	push af
 	ld a, d
